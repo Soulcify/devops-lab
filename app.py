19621c96eb40cc33377b5ddbd0538c7ca54f6122
Rafael Lima <69235772+Soulcify@users.noreply.github.com>
2025-09-15 15:35:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-def hello():
+from flask import Flask
+
+def get_message():
     return "Hello, DevOps!"
 
+def create_app():
+    app = Flask(__name__)
+
+    @app.get("/")
+    def hello():
+        return get_message()
+
+    return app
+
 if __name__ == "__main__":
-    print(hello())
+    app = create_app()
+    app.run(host="0.0.0.0", port=8000)
